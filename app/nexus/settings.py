3129c97dc63744d08be5e73ad8be4477ac9b4af1
Alexis <alcamoru@icloud.com>
2024-02-21 16:38:31 +1100

    Restarting from scratch. Getting a clean development project before starting deployment

--- app/nexus/settings.py
+++ app/nexus/settings.py
@@ -22,16 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY", "changeme")
+SECRET_KEY = "changeme"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool(int(os.environ.get("DEBUG", 0)))
-
-ALLOWED_HOSTS = []
-ALLOWED_HOSTS_ENV = os.environ.get("ALLOWED_HOSTS")
-
-if ALLOWED_HOSTS_ENV:
-    ALLOWED_HOSTS.extend(ALLOWED_HOSTS_ENV.split(","))
+DEBUG = True
 
 
 # Application definition
