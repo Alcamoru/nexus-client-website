f5efe51ad8c8e59fb3e9e91739419c60d589f5ac
alcam <alcamoru@icloud.com>
2024-03-17 14:46:58 +0100

    Add docker-compose.yml to .gitignore and changed settings.py

--- app/nexus/settings.py
+++ app/nexus/settings.py
@@ -21,8 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open("./DJANGO_TOKEN.txt", "r") as token:
-    SECRET_KEY = token.read()
+SECRET_KEY = 'changeme'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
